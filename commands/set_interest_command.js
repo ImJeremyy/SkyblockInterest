23b2063ee964d0ac9f7baf653119d6a2b9309fa5
ImJeremyy <madivoso@gmail.com>
2020-09-13 21:59:19 -0400

    a

--- commands/set_interest_command.js
+++ commands/set_interest_command.js
@@ -31,6 +31,14 @@ class SetInterestCommand extends CommandBase {
 					if (!isNaN(hours) && !isNaN(minutes)) {
 						client.set("hoursLeft", hours);
 						client.set("minutesLeft", minutes);
+
+						console.log("set!");
+						client.get("hoursLeft", (err, reply) => {
+							console.log("hoursLeft: " + reply.toString());
+						});
+						client.get("minutesLeft", (err, reply) => {
+							console.log("minutesLeft: " + reply.toString());
+						});
 						channel.send(user.toString() + " Set time left to next interest: " + time + " (Time will sync to 31 hrs after this period)");
 					} else {
 						channel.send(user.toString() + " " + args[1] + " is not a number!");
